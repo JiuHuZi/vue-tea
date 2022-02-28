3a934e3e66f0d0d3573f328599f21d3e1b69147d
九狐子 <1225247658@qq.com>
2022-02-28 22:52:50 +0800

    update

--- public/db/vue_shop.sql
+++ public/db/vue_shop.sql
@@ -16,6 +16,25 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`vue_shop` /*!40100 DEFAULT CHARACTER SE
 
 USE `vue_shop`;
 
+/*Table structure for table `goods_cart` */
+
+DROP TABLE IF EXISTS `goods_cart`;
+
+CREATE TABLE `goods_cart` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `goods_id` int(11) NOT NULL,
+  `goods_name` varchar(200) NOT NULL,
+  `goods_price` double NOT NULL,
+  `goods_num` int(11) NOT NULL,
+  `goods_imgUrl` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `goods_cart` */
+
+insert  into `goods_cart`(`id`,`uid`,`goods_id`,`goods_name`,`goods_price`,`goods_num`,`goods_imgUrl`) values (1,6,3,'黄山太平猴魁绿茶1号',99,1,'/images/goods3.jpeg'),(2,6,7,'金油滴建盏',298,1,'/images/goods7.jpeg'),(3,6,1,'浅尝-金牡丹（武夷岩茶）',14.6,1,'/images/goods1.jpeg'),(4,6,9,'2016白毫银针巧克力茶砖',98,1,'/images/goods9.jpeg'),(5,8,9,'2016白毫银针巧克力茶砖',98,1,'/images/goods9.jpeg');
+
 /*Table structure for table `goods_list` */
 
 DROP TABLE IF EXISTS `goods_list`;
@@ -28,6 +47,7 @@ CREATE TABLE `goods_list` (
   `num` int(11) DEFAULT NULL,
   `imgUrl` varchar(200) DEFAULT NULL,
   `complete` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 
@@ -47,11 +67,11 @@ CREATE TABLE `user` (
   `nickName` varchar(200) DEFAULT NULL,
   `token` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `user` */
 
-insert  into `user`(`id`,`tel`,`pwd`,`imgUrl`,`nickName`,`token`) values (1,'13144274876','123456','/images/headerImg/jiuhu.jpg','九狐',NULL),(2,'12345678901','888888','/images/headerImg/header.jpeg','张三',NULL),(3,'13166981478','666666','/images/headerImg/header.jpeg','李四',NULL),(4,'13144112259','999999','/images/headerImg/header.jpeg','王五',NULL),(5,'17345698789','666666','/images/headerImg/header.jpeg','赵六',''),(6,'13144274877','666666','/images/headerImg/header.jpeg','孙七','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWwiOiIxMzE0NDI3NDg3NyIsImlhdCI6MTY0NjAzNTUzMn0.SBcn8kcsSfJQIZD7aDNnpgCsaN29ht6AcEJHTsdcCrI');
+insert  into `user`(`id`,`tel`,`pwd`,`imgUrl`,`nickName`,`token`) values (2,'12345678901','888888','/images/headerImg/header.jpeg','张三',NULL),(3,'13166981478','666666','/images/headerImg/header.jpeg','李四',NULL),(4,'13144112259','999999','/images/headerImg/header.jpeg','王五',NULL),(5,'17345698789','666666','/images/headerImg/header.jpeg','赵六',''),(6,'13144274877','666666','/images/headerImg/header.jpeg','孙七','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWwiOiIxMzE0NDI3NDg3NyIsImlhdCI6MTY0NjAzNTUzMn0.SBcn8kcsSfJQIZD7aDNnpgCsaN29ht6AcEJHTsdcCrI'),(7,'12345678999','666666','/images/headerImg/header.jpeg','12345678999','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWwiOiIxMjM0NTY3ODk5OSIsImlhdCI6MTY0NjA0NTY0NX0.no5zXsIsie5jXTE-SlxPF96yPnrJg8i718RpnD70df8'),(8,'13144274876','666666','/images/headerImg/header.jpeg','13144274876','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWwiOiIxMzE0NDI3NDg3NiIsImlhdCI6MTY0NjA1MzM3Mn0.jzO2m4exthzFXo5ZnLP_hx3O0Rt87SGoReNBEfPEtec');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
